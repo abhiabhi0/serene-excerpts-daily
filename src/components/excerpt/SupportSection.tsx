7496219f74f8052424f625b3e448e78c05b36e07
Abhishek Singh Thakur <mailabhi593@gmail.com>
2025-02-10 11:03:46 +0530

    remove unnecessay text

--- src/components/excerpt/SupportSection.tsx
+++ src/components/excerpt/SupportSection.tsx
@@ -38,7 +38,7 @@ export const SupportSection = () => {
               className="flex items-center justify-center gap-2 w-full px-4 py-2 bg-[#1A4067] hover:bg-[#1A4067]/80 rounded-md transition-colors"
             >
               <IndianRupee className="w-4 h-4" />
-              <span>Copy UPI ID: atmanamviddhi@axl</span>
+              <span>atmanamviddhi@axl</span>
             </button>
           </div>
         </div>
